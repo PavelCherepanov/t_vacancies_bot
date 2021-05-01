d9ed3a3a2afbe52dc85bd45ff46fc9eecf2d99c3
PavelCherepanov <pavelk.cherepanov@gmail.com>
2021-05-01 11:42:04 +0700

    second commit

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
+# -*- coding: utf-8 -*-
+
 import telebot
-#import parser.
+import json
+
 TOKEN = ""
 
 
+with open("vacancies.json", "r") as read_file:
+    data = json.load(read_file)
+
 bot = telebot.TeleBot(TOKEN)
+
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
   if message.text == "Привет":
       bot.send_message(message.from_user.id, "Привет, сейчас я покажу тебе все вакасии.")
+      bot.send_message(message.from_user.id, json.dumps(data))
+
   elif message.text == "/help":
       bot.send_message(message.from_user.id, "Напиши Привет")
   else:
--- parser.py
+++ parser.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import requests
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup
+import json
+import string
 
 URL = "https://www.weblancer.net/jobs/?page="
 
@@ -7,21 +13,26 @@ headers = {
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7",
 }
 
-# print(r, end="\n\n")
 
 num_of_page = 10
+
 
 for i in range(num_of_page):
 	url = URL + str(i + 1)
 	r = requests.get(url, headers=headers) 
 	soup = BeautifulSoup(r.text, "html.parser")
-	vacances = soup.find_all("a", {"class": "text-bold show_visited"})
+	vacancies = soup.find_all("a", {"class": "text-bold show_visited"})
 
 
 
-	for v in vacances:
-		print(v.text)	
-		print(URL + v.get("href"), end="\n\n")
+	for v in vacancies:
+		# print(v.text)	
+		# print(, end="\n\n")
+		# URL + v.get("href")
+		data = dict.fromkeys(v.text, URL + v.get("href"))
+
+	with open("vacancies.json", "w", encoding='utf-8') as write_file:
+		json.dump(data, write_file)
  
  
 
